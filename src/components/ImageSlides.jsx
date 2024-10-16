4384fbec43d9de24c1e8aa6c21a7b968a2a0c5e5
GiaKiet <nguyentrangiakiet47@gmail.com>
2024-10-16 19:45:16 +0700

    Fix ImageSlides and add banner

--- src/components/ImageSlides.jsx
+++ src/components/ImageSlides.jsx
@@ -1,16 +1,44 @@
 import Carousel from 'react-bootstrap/Carousel';
 
-function ImageSlides() {
+function ImageSlides({ height = '100%', width = '100%', interval = 3000, imgStyles = {} }) {
+    const styles = {
+        imageSlides: {
+            height: height,
+            overflow: 'hidden',
+        },
+        imageSlideItem: {
+            img: {
+                width: '100%',
+                objectFit: 'cover',
+                objectPosition: 'center',
+                ...imgStyles,  // Allows additional custom styles to be passed from parent
+            },
+        },
+    };
+
     return (
-        <Carousel id='imageSlides' interval={null}>
-            <Carousel.Item className='imageSlideItem'>
-                <img className='rounded' src="images/winx.jpg" alt="" />
+        <Carousel id="imageSlides" style={styles.imageSlides} interval={interval}>
+            <Carousel.Item className="imageSlideItem">
+                <img
+                    className="rounded"
+                    style={styles.imageSlideItem.img}
+                    src="images/winx.jpg"
+                    alt="slide 1"
+                />
             </Carousel.Item>
-            <Carousel.Item className='imageSlideItem'>
-                <img src="images/winx.jpg" alt="" />
+            <Carousel.Item className="imageSlideItem">
+                <img
+                    style={styles.imageSlideItem.img}
+                    src="images/winx.jpg"
+                    alt="slide 2"
+                />
             </Carousel.Item>
-            <Carousel.Item className='imageSlideItem'>
-                <img src="images/winx.jpg" alt="" />
+            <Carousel.Item className="imageSlideItem">
+                <img
+                    style={styles.imageSlideItem.img}
+                    src="images/winx.jpg"
+                    alt="slide 3"
+                />
             </Carousel.Item>
         </Carousel>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ProductsContainer from '../components/Products';
-import Info from '../components/Info'
+import ImageSlides from '../components/ImageSlides';
 
 export default function Home() {
-    return (
-      <div className="homePage container">
-        <Info />
-          <ProductsContainer />
-      </div>
-    );
+  const slideImgStyles = {
+    borderRadius: '10px',
+  };
+
+  return (
+    <div className="homePage container">
+      <ImageSlides
+        height="400px" width='100%' interval={null} imgStyles={slideImgStyles}
+      />
+      <ProductsContainer />
+    </div>
+  );
 }
