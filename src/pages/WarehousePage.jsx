028315bbc9343f8c280eab70d27f5782e6c6dad2
Sang <sanqsanq2000@gmail.com>
2024-11-04 10:25:17 +0700

    Cap Nhap Warehouse

--- src/pages/WarehousePage.jsx
+++ src/pages/WarehousePage.jsx
@@ -3,7 +3,7 @@ import Warehouse from '../components/Warehouse';
 export default function WarehousePage() {
     return (
         <Col sm={12} md={12} lg={10} xl={10}>
-            <h1>Quản lý kho hàng</h1>
+            <h1 className="text-center mb-5">Quản lý kho hàng</h1>
             <Warehouse />
         </Col>
     );
